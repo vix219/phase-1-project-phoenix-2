8c957d73f6a2af4d7c9eccffd7e98b0ccee5eba4
VictoriaCarvalho <vix219@icloud.com>
2025-02-10 10:28:15 -0600

    api fetch

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ function getAllMusic() {
 fetch('http://localhost:3000/tracks')
     .then(response => response.json())
     .then(response => console.log(response))
+    tracks.forEach(tracks => {
+        let img = document.querySelector('img');
+        img.src = tracks.img;
+        img.id = tracks.name;
+        img.genre = tracks.pl.name;
+    })
 }
 
 function search() {
     const paragraph = document.getElementById("p");
     const artistName = document.getElementById("usernameInput").value;
     paragraph.innerHTML = artistName + " Searching!";
+    artistName.addEventListener('click', () => search(tracks))
+    let artistSearch = {
+        name: document.getElementById('').value,  
+        genre: document.getElementById('').value,  
+        image: document.getElementById('').value,  
+}
 
 }
+
