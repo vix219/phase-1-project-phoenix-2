ad58de6bdfeb8734cc64dcee7da249f9c9ddd464
VictoriaCarvalho <vix219@icloud.com>
2025-02-10 14:34:05 -0600

    added pseudo code

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+
+// Create a function that fetches all data from db.json file
+// Add button in HTML file that searches through artist, or genre
+// Create a function that displays and plays music of searched item
+//       - In that function display the information for the music: Artist name, Song name, Genre, Link to video.
+// Create an HTML button for "likes 👍🏼" and one for "dislikes 👎🏼"
+// Create a function that has a button for "likes", and eventListener for the "likes button".
+
+// Create a form with id of "Contribute to Music Box" in HTML that has
+//       - key:values of Artist name, Song name, Genre, Link to song. It has a form button of "   ".
+// Create a function that has an evenListener for the "Contribute to Music Box"
+// Remember to e.preventDefault(), e.target.reset() 
+// Invoke functions and append!
+// Make a fetch() with method oof 'POST' to add the new music with key:values from form to the db.json objects
+
+
+
+
 function getAllMusic() {
 fetch('http://localhost:3000/tracks')
     .then(response => response.json())
