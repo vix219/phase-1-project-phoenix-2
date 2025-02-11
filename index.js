a0f933f2b35edc335b369825bea2a0a5ca1dd714
Bruno <bltrova@gmail.com>
2025-02-11 08:55:17 -0800

    js-search-function

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
-const tracks = document.querySelector();
-const image = document.querySelector();
-const genre = document.querySelector();
-const song = document.querySelector();
-const likes = document.querySelector();
-=======
+// const tracks = document.querySelector();
+// const image = document.querySelector();
+// const genre = document.querySelector();
+// const song = document.querySelector();
+// const likes = document.querySelector();
 
 // Create a function that fetches all data from db.json file
 // Add button in HTML file that searches through artist, or genre
@@ -21,20 +19,19 @@ const likes = document.querySelector();
 // Make a fetch() with method oof 'POST' to add the new music with key:values from form to the db.json objects
 
 
->>>>>>> main
-
-
 function getAllMusic() {
     fetch('./db.json')
     .then((response) => response.json()) 
     .then(data => {
       console.log(data);
+      /*
       tracks.forEach(tracks => {
         let img = document.querySelector('img');
         img.src = tracks.pl.img;
         // img.id = track.name;
         img.genre = tracks.pl.name;
       });
+      */
     });
     /*
     fetch('./db.json')
@@ -48,23 +45,58 @@ function getAllMusic() {
     });
     */
 }
+
+function getMusicByArtits(artistName) {
+  console.log('artistName: ', artistName);
+  fetch('./db.json')
+  .then((response) => response.json()) 
+  .then(data => {
+    console.log(data);
+    // data.tracks.filter((tracks) => tracks.uNm === artistName)
+    // .map((items) => {
+    //   console.log(items);
+    // })
+    data.tracks.forEach((tracks) => {
+      if (tracks.uNm === artistName) {
+        console.log(tracks);
+        /*
+        let img = document.querySelector('img');
+        img.src = tracks.pl.img;
+        // img.id = track.name;
+        img.genre = tracks.pl.name;
+        */
+      }
+    });
+  });
+}
+
+function searchArtist() {
+  console.log('search');
+  const paragraph = document.getElementById("message-artist");
+  const artistNameInput = document.getElementById("search-artist-button");
+  console.log(artistNameInput);
+  const artistName = document.getElementById("searchInput").value;
+  paragraph.innerHTML = "Searching " + "for " + artistName;
+  artistNameInput.addEventListener('click', () => getMusicByArtits(artistName))
+  // let artistSearch = {
+  //     name: document.getElementById('').value,  
+  //     genre: document.getElementById('').value,  
+  //     image: document.getElementById('').value,  
+  // }
+}
+
+// function artist() {
+// }
+
+// function genre() {
+// }
+
+function main() {
+  const callButton = document.getElementById('search-artist-button');
+  callButton.addEventListener('click', () => searchArtist());
+}
+
+main();
 getAllMusic();
-
-function search() {
-    const paragraph = document.getElementById("p");
-    const artistName = document.getElementById("usernameInput").value;
-    paragraph.innerHTML = artistName + " Searching!";
-    artistName.addEventListener('click', () => search(tracks))
-    let artistSearch = {
-        name: document.getElementById('').value,  
-        genre: document.getElementById('').value,  
-        image: document.getElementById('').value,  
-    }
-}
-function artist() {
-
-}
-
-function genre() {
-    
-}
+getMusicByArtits();
+// searchArtist();
