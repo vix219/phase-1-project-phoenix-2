23c01c77c3d46bbe0b0f639b2336adedb27dab8d
VictoriaCarvalho <vix219@icloud.com>
2025-02-12 10:23:22 -0600

    ADD: js vid gallery, HTML iframe, CSS scroll feature

--- index.js
+++ index.js
@@ -19,11 +19,11 @@
 // Make a fetch() with method oof 'POST' to add the new music with key:values from form to the db.json objects
 
 
-function getAllMusic() {
-    fetch('./db.json')
-    .then((response) => response.json()) 
-    .then(data => {
-      console.log(data);
+// function getAllMusic() {
+//     fetch('./db.json')
+//     .then((response) => response.json()) 
+//     .then(data => {
+//       console.log(data);
       /*
       tracks.forEach(tracks => {
         let img = document.querySelector('img');
@@ -32,7 +32,7 @@ function getAllMusic() {
         img.genre = tracks.pl.name;
       });
       */
-    });
+    // });
     /*
     fetch('./db.json')
     .then(response => console.log(response.json()))
@@ -44,90 +44,122 @@ function getAllMusic() {
         img.genre = tracks.pl.name;
     });
     */
-}
+// }
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-        let i;
-        let slides = document.getElementsByClassName("mySlides");
-        let dots = document.getElementsByClassName("demo");
-        let captionText = document.getElementById("caption");
-              if (n > slides.length) {slideIndex = 1}
-              if (n < 1) {slideIndex = slides.length}
-              for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-              }
-              for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-              }
-              slides[slideIndex-1].style.display = "block";
-              dots[slideIndex-1].className += " active";
-              captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
-
-function playMusic(tracks) {
-  const videoEle = document.querySelector('video');
-  const imgEle = document.querySelector('img');
+// function playMusic(tracks) {
+//   const videoEle = document.querySelector('video');
+//   const imgEle = document.querySelector('img');
   
-  tracks.forEach(track => {
-    if (videoEle && imgEle) {
-      videoEle.src = track.trackUrl; 
-      imgEle.src = track.img;
-      videoEle.addEventListener('click', () => {
-        videoEle.play();
-        videoEle.defaultMuted = true;
-        console.log(videoEle.outerHTML); 
-      });
-    }
-  });
-}
+//   tracks.forEach(track => {
+//     if (videoEle && imgEle) {
+//       videoEle.src = track.trackUrl; 
+//       imgEle.src = track.img;
+//       videoEle.addEventListener('click', () => {
+//         videoEle.play();
+//         videoEle.defaultMuted = true;
+//         console.log(videoEle.outerHTML); 
+//       });
+//     }
+//   });
+// }
 
-function getMusicByArtist(artistName) {
-  console.log('Searching for artist: ', artistName);
-  fetch('./db.json')
-    .then(response => response.json())
-    .then(data => {
-      // Filter tracks by the artist
-      const artistTracks = data.filter(track => track.artist.toLowerCase() === artistName.toLowerCase());
+// function getMusicByArtist(artistName) {
+//   console.log('Searching for artist: ', artistName);
+//   fetch('./db.json')
+//     .then(response => response.json())
+//     .then(data => {
+//       // Filter tracks by the artist
+//       const artistTracks = data.filter(track => track.artist.toLowerCase() === artistName.toLowerCase());
 
-      if (artistTracks.length > 0) {
-        playMusic(artistTracks); // Play the music related to the artist
-      } else {
-        console.log('No tracks found for artist: ', artistName);
-      }
-    })
-    .catch(error => console.error('Error fetching tracks:', error));
-}
+//       if (artistTracks.length > 0) {
+//         playMusic(artistTracks); // Play the music related to the artist
+//       } else {
+//         console.log('No tracks found for artist: ', artistName);
+//       }
+//     })
+//     .catch(error => console.error('Error fetching tracks:', error));
+// }
 
-function searchArtist() {
-  console.log('search initiated');
-  const paragraph = document.getElementById("message-artist");
-  const artistNameInput = document.getElementById("search-artist-button");
-  const artistName = document.getElementById("searchInput").value;
+// function searchArtist() {
+//   console.log('search initiated');
+//   const paragraph = document.getElementById("message-artist");
+//   const artistNameInput = document.getElementById("search-artist-button");
+//   const artistName = document.getElementById("searchInput").value;
 
-  paragraph.innerHTML = "Searching for " + artistName;
+//   paragraph.innerHTML = "Searching for " + artistName;
   
-  // Trigger the search
-  getMusicByArtist(artistName);
-}
+//   // Trigger the search
+//   getMusicByArtist(artistName);
+// }
 
 function main() {
-  const callButton = document.getElementById('search-artist-button');
-  callButton.addEventListener('click', () => searchArtist());
-  showSlides();
-  playMusic();
+  // const callButton = document.getElementById('search-artist-button');
+  // callButton.addEventListener('click', () => searchArtist());
+  // playMusic();
   
 }
 
 main();
+
+const galleryContainer = document.getElementById('gallery-container');
+
+// Function to get youTube video id's from URL
+function getYouTubeVideoId(url) {
+  if (url.includes('youtu.be')) {
+    return url.split('/').pop();
+  }
+  return null;
+}
+
+// Function to create the iframe for each video in the browser display
+function createIframe(videoId) {
+  const iframe = document.createElement('iframe');
+  iframe.classList.add('video-src');
+  iframe.width = 300;
+  iframe.height = 250;
+  iframe.controls = true;
+  iframe.src = `https://www.youtube.com/embed/${videoId}`;
+
+  iframe.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+  iframe.allowFullscreen = true;
+  return iframe;
+}
+
+// Function to create the description for each video
+function createDescription(artist, song, likes) {
+  const descDiv = document.createElement('div');
+  descDiv.classList.add('desc');
+  descDiv.innerHTML = `${artist} <br> ${song} <br>  ${likes} 👍🏼`;
+  return descDiv;
+}
+
+//Function to display the music from the db.json file videos
+function displayMusic() {
+  fetch('./db.json')
+    .then(response => response.json())
+    .then(data => {
+      const tracks = data.tracks;
+      galleryContainer.innerHTML = ''; 
+      tracks.forEach(track => {
+        const videoId = getYouTubeVideoId(track.link);
+        if (videoId) {
+          const iframe = createIframe(videoId);
+          const descDiv = createDescription(track.artist, track.song, track.likes);
+
+        
+          const galleryDiv = document.createElement('div');
+          galleryDiv.classList.add('gallery');
+          galleryDiv.appendChild(iframe);
+          galleryDiv.appendChild(descDiv);
+
+          galleryContainer.appendChild(galleryDiv);
+        } else {
+          console.error('Invalid YouTube URL:', track.link);
+        }
+      });
+    })
+    .catch(error => console.error(error));
+}
+
+//Invoke the function to load data
+window.onload = displayMusic;
